f82fd305db2e7bfde506d5effe7040a6be4afeb4
Leon van Wijngaarden <102827@glr.nl>
2025-02-16 23:13:38 +0000

    Homepage Api + 3d model with a turn off and turn on auto rotate function

--- javascript/index.js
+++ javascript/index.js
@@ -1,35 +1,62 @@
-const apiKey = "e513f8f350bd40b44869979a19213b16"; // Vervang met je eigen API-sleutel
-const city = "Amsterdam"; // Stel de stad in waarvoor je het weer wilt ophalen
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=nl`;
+let apiKey = "e513f8f350bd40b44869979a19213b16";
+let city = "Amsterdam";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=nl`;
 
-//weer te halen en weer te geven
 async function getWeather() {
     try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
+        let response = await fetch(apiUrl);
+        let data = await response.json();
 
-        // Haal de benodigde informatie uit de API-respons
-        const temperature = data.main.temp;
-        const weatherDescription = data.weather[0].description;
-        const humidity = data.main.humidity;
-        const windSpeed = data.wind.speed;
+        let temperature = data.main.temp;
+        let weatherDescription = data.weather[0].description;
+        let humidity = data.main.humidity;
+        let windSpeed = data.wind.speed;
 
-        // Maak een tekst die het weer samenvat
-        const weatherContent = `
-            <h1> Ik kan api gebruiken</h1>
-            <h2>Het weer in ${city}</h2>
-            <p>Temperatuur: ${temperature}°C</p>
-            <p>Beschrijving: ${weatherDescription}</p>
-            <p>Luchtvochtigheid: ${humidity}%</p>
-            <p>Windsnelheid: ${windSpeed} m/s</p>
-        `;
-
-        // Toon de weerinformatie op de pagina
-        document.getElementById("weather").innerHTML = weatherContent;
+        document.getElementById("city").innerText = `Het weer in ${city}`;
+        document.getElementById("temperature").innerText = `Temperatuur: ${temperature}°C`;
+        document.getElementById("description").innerText = `Beschrijving: ${weatherDescription}`;
+        document.getElementById("humidity").innerText = `Luchtvochtigheid: ${humidity}%`;
+        document.getElementById("windSpeed").innerText = `Windsnelheid: ${windSpeed} m/s`;
     } catch (error) {
         console.error("Fout bij het ophalen van weerdata:", error);
     }
 }
-
-// Haal het weer op bij het laden van de pagina
 getWeather();
+
+let rotate = false;
+
+function HandleRotate() {
+    rotate = !rotate;
+
+    const md = document.getElementById("mdl-34");
+    if (!md) return;
+
+    console.log(rotate);
+
+    md.innerHTML = loadModel(rotate);
+}
+
+function loadModel(rotate) {
+    if (rotate) {
+        return `
+        <model-viewer 
+            id="modelViewer"
+            src="./models/office_computer.glb" 
+            alt="Retro Computer" 
+            auto-rotate 
+            camera-controls
+            class="w-full h-[300px] mt-4">
+        </model-viewer>
+        `;
+    }
+
+    return `
+        <model-viewer 
+            id="modelViewer"
+            src="./models/office_computer.glb" 
+            alt="Retro Computer" 
+            camera-controls
+            class="w-full h-[300px] mt-4">
+        </model-viewer>
+        `;
+}
